a60bafaa08c577ac0fb1c305c78e69ad7b015f55
pradt2 <12902844+pradt2@users.noreply.github.com>
2023-04-06 01:28:26 +0100

    3200 0allocs /s

--- alloc_min.rs
+++ alloc_min.rs
@@ -1,19 +1,23 @@
-use std::alloc::Layout;
+mod memset;
+
+use std::alloc::{alloc, Layout};
 use std::collections::LinkedList;
-use std::ptr::slice_from_raw_parts_mut;
+use std::env;
+use std::mem::{align_of, size_of};
+use std::ptr::{slice_from_raw_parts_mut, write_volatile};
 
 const HEAP_PAGE_SIZE_WORDS: usize = 128000;
-const SMALL_ALLOC_WORDS: usize = 32 + 1;
-const HEAP_PAGE_MIN_FREE: usize = HEAP_PAGE_SIZE_WORDS - SMALL_ALLOC_WORDS;
 
-fn layout() -> Layout {
-    Layout::array::<u64>(HEAP_PAGE_SIZE_WORDS).unwrap()
+type WORD = usize;
+
+unsafe fn layout(size: WORD) -> Layout {
+    Layout::from_size_align_unchecked(size_of::<WORD>() * size, align_of::<WORD>())
 }
 
 
 struct Allocator {
-    pages: LinkedList<*mut u64>,
-    curr_page_words_used: usize,
+    pages: LinkedList<*mut WORD>,
+    curr_page_words_used: WORD,
 }
 
 impl Allocator {
@@ -24,47 +28,67 @@ impl Allocator {
             curr_page_words_used: 0,
         };
 
-        allocator.add_page();
+        allocator.add_page(HEAP_PAGE_SIZE_WORDS);
 
         allocator
     }
 
-    unsafe fn add_page(&mut self) {
-        self.pages.push_back(std::mem::transmute(std::alloc::alloc(layout())));
-        self.curr_page_words_used = 0;
+    unsafe fn add_page(&mut self, size: WORD) {
+        let page_ptr: *mut WORD = std::mem::transmute(std::alloc::alloc(layout(size)));
+        self.pages.push_back(page_ptr);
+
+        // it seems performance beneficial to write to the newly allocated page
+        // even if the value is not used later
+        *page_ptr = size;
+
+        if cfg!(feature = "page-size-tracking") {
+            self.curr_page_words_used = 1;
+        } else {
+            self.curr_page_words_used = 0;
+        }
     }
 
-    unsafe fn alloc_small(&mut self, typ: u64, size: usize) -> *const u64 {
-        if self.curr_page_words_used > HEAP_PAGE_MIN_FREE {
-            self.add_page();
+    unsafe fn alloc<const TYP: WORD, const SIZE: WORD>(&mut self) -> *const WORD {
+        if self.curr_page_words_used > HEAP_PAGE_SIZE_WORDS - SIZE - 2 {
+            self.add_page(HEAP_PAGE_SIZE_WORDS)
         }
 
         let last_page = *self.pages.back_mut().unwrap();
 
         *last_page.add(self.curr_page_words_used) = std::mem::transmute(last_page);
-        *last_page.add(self.curr_page_words_used + 1) = typ;
-        (*slice_from_raw_parts_mut(last_page.add(self.curr_page_words_used + 2), size)).fill(0x00);
+        *last_page.add(self.curr_page_words_used + 1) = TYP;
 
-        let alloc = last_page.add(2);
+        let alloc = last_page.add(self.curr_page_words_used + 2);
 
-        self.curr_page_words_used += 2 + size;
+        for offset in 0..SIZE {
+            alloc.add(offset).write(0);
+        }
+
+        self.curr_page_words_used += 2 + SIZE;
 
         alloc
     }
 
     unsafe fn wipe(&mut self) {
         for page in self.pages.iter() {
-            std::alloc::dealloc(std::mem::transmute(*page), layout());
+            let size = if cfg!(feature = "page-size-tracking") {
+                **page as usize
+            } else {
+                0
+            };
+            std::alloc::dealloc(std::mem::transmute(*page), layout(size));
         }
         self.pages.clear();
-        self.add_page();
+        self.add_page(HEAP_PAGE_SIZE_WORDS);
     }
 }
 
-const SIZE: u64 = 1024;
-const ITERS: u64 = 1024 * 256 * 3;
+const SIZE: WORD = 1024;
+const ITERS: WORD = 1024 * 256 * 3;
 
 fn main() {
+    let size = env::args().len() * 2;
+
     unsafe {
         let mut alloc = Allocator::new();
 
@@ -72,7 +96,7 @@ fn main() {
 
         for iter in 0..SIZE {
             for _i in 0..ITERS {
-                alloc.alloc_small(1, 2);
+                alloc.alloc::<1, 0>();
             }
             if iter % 1 == 0 {
                 alloc.wipe();
@@ -81,7 +105,7 @@ fn main() {
 
         let end = std::time::Instant::now();
 
-        let ms = end.checked_duration_since(start).unwrap().as_millis() as u64;
+        let ms = end.checked_duration_since(start).unwrap().as_millis() as WORD;
 
         println!("{}", ms);
         println!("Allocs per sec (in millions) {}", ((SIZE * ITERS) / ms * 1000) / 1000000);
